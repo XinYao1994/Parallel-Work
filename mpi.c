3aec50de9823d6877ae6b6c658734ecc1c6be1a3
XinYao1994 <XinYao1994@users.noreply.github.com>
2015-06-23 20:06:27 +0800

    mpi

--- mpi.c
+++ mpi.c
@@ -84,8 +84,8 @@ void init_con(int id, int same){
     }
     //Recv
     for(i=bb;i<ee;i++){
-        MPI_Recv(bufa, sizeof(bufa), MPI_INT, i, 1, MPI_COMM_WORLD, status);
-        MPI_Recv(bufb, sizeof(bufb), MPI_INT, i, 2, MPI_COMM_WORLD, status);
+        MPI_Recv(bufa, sizeof(bufa), MPI_INT, i, 1, MPI_COMM_WORLD, &status);
+        MPI_Recv(bufb, sizeof(bufb), MPI_INT, i, 2, MPI_COMM_WORLD, &status);
         //fill in
         //xstep ystep not change
         con = 0;
@@ -111,8 +111,8 @@ void init_con(int id, int same){
     int *buffa = (int *)malloc(sizeof(bufa));
     int *buffb = (int *)malloc(sizeof(bufb));
     for(i=bb;i<ee;i++){
-        MPI_Recv(buffa, sizeof(buffa), MPI_INT, i, 1, MPI_COMM_WORLD, status);
-        MPI_Recv(buffb, sizeof(buffb), MPI_INT, i, 2, MPI_COMM_WORLD, status);
+        MPI_Recv(buffa, sizeof(buffa), MPI_INT, i, 1, MPI_COMM_WORLD, &status);
+        MPI_Recv(buffb, sizeof(buffb), MPI_INT, i, 2, MPI_COMM_WORLD, &status);
         //fill in
         //xstep ystep not change
         con = 0;
@@ -215,7 +215,8 @@ int main(int argc, char **argv){
     init_con(Pid, lb);
     count(Pid, lc, la, lb);
     allocMm(&c, la, lc);
-    for(int i=0;i<nPNum;i++){
+    int i;
+    for(i=0;i<nPNum;i++){
         fill(i, lc, la, status);
     }
   }
