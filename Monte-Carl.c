b20edbbe5109d65191fc060f9d280d61a80c7d55
XinYao1994 <XinYao1994@users.noreply.github.com>
2015-06-24 22:07:49 +0800

    Monte-Carl

--- Monte-Carl.c
+++ Monte-Carl.c
@@ -7,7 +7,7 @@
 #define NumPro 16
 #define Ran (rand()%(BORD+1))
 
-int bufa[NumPro], bufb[NumPro];
+int buf[NumPro];
 
 int Myatoi(char *c){
     int sum = 0;
@@ -19,9 +19,8 @@ int Myatoi(char *c){
     return sum;
 }
 
-int *chip;
 int nPNum, Pid;
-int Ycon, Fcon;
+int Ycon;
 
 int fun(int x){
     return x*x;
@@ -35,14 +34,8 @@ void test(int t){
         y = Ran;
         #pragma omp critical
         {
-            *(chip + y*BORD + x) = *(chip + y*BORD + x) + 1;
             if(y <= fun(x)) Ycon++;
         }
-    }
-    #pragma omp parallel for reduction(+:Fcon)
-    for(i=0; i<=BORD; i++){
-        for(j=0; j<=BORD; j++)
-         Fcon += *(chip+i*BORD+j);
     }
 }
 
@@ -57,24 +50,19 @@ int main(int argc, char **argv){
     else all = CON;
     //alloc a board
     int b = BORD + 1;
-    chip = (int *)malloc(sizeof(int)*b*b);
-    memset(chip, 0, sizeof(int)*b*b);
-    Ycon = Fcon = 0;
+    Ycon = 0;
     if(Pid==0){
         test(all/NumPro);
         int i;
         bufa[0] = Ycon;
         bufb[0] = Fcon;
         for(i=1; i<NumPro; i++){
-            MPI_Recv(bufa+i, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
-            MPI_Recv(bufb+i, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
+            MPI_Recv(buf+i, 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
         }
         int sum = 0;
-        all = 0;
         #pragma omp parallel for reduction(+:sum, all)
         for(i=0; i<NumPro; i++){
-            sum += bufa[i];
-            all += bufb[i];
+            sum += buf[i];
         } 
         printf("the area of fun from [0,%d] is %.2f",BORD,(double)(sum)/(double)(all)*(double)(BORD*BORD));
     }
@@ -82,7 +70,7 @@ int main(int argc, char **argv){
         test(all/NumPro);
         MPI_Finalize();
         MPI_Send(&Ycon, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-        MPI_Send(&Fcon, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+        MPI_Finalize();
         return 0;
     }
     MPI_Finalize();
